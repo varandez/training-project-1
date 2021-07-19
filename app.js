376136fa66f8165ca4f6a0a1b025b7c6eae3623f
varandez <varandez@gmail.com>
2021-07-19 20:02:05 +0300

    Добавил условие проверки количества просмотренных фильмов

--- app.js
+++ app.js
@@ -36,3 +36,13 @@ for (let i = 0; i < 2; i++) {
         i--;
     }
 }
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
