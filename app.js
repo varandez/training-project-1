e611329f40e6d832936cb18f83c8d73653f0ca72
varandez <varandez@gmail.com>
2021-07-22 22:47:10 +0300

    Добавляет функцию detectPersonaLevel

--- app.js
+++ app.js
@@ -50,14 +50,17 @@ function rememberMyFilms() {
 
 rememberMyFilms();
 
-
-
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
+function detectPersonaLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
 }
+
+detectPersonaLevel();
+
